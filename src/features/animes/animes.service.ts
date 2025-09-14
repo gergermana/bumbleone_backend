96bb624bd063971b46d9e4c1680819e9de6b7ae6
Htoo Akar Lin <180033134+gergermana@users.noreply.github.com>
2025-09-15 00:54:03 +0630

    animes findAll route modified

--- src/features/animes/animes.service.ts
+++ src/features/animes/animes.service.ts
@@ -8,88 +8,101 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class AnimesService {
   constructor(private prisma: PrismaService) {}
 
-  create(createAnimeDto: CreateAnimeDto) {
-    const { genres, studios, ...rest } = createAnimeDto;
+    create(createAnimeDto: CreateAnimeDto) {
+        const { genres, studios, ...rest } = createAnimeDto;
 
-    return this.prisma.anime.create({
-      data: {
-        ...rest,
-        genres: genres 
-        ? {
-            connect: genres?.map((id) => ({ id })),
-          }
-        : undefined,
-        studios: studios 
-        ? {
-            connect: studios?.map((id) => ({ id })),
-          }
-        : undefined,
-      },
-      include: {
-        genres: true,
-        studios: true,
-      },
-    });
-  }
-
-  async findAll(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.AnimeWhereUniqueInput;
-    where?: Prisma.AnimeWhereInput;
-    orderBy?: Prisma.AnimeOrderByWithRelationInput;
-  }): Promise<{ datalist: Anime[]; total: number }> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return {
-      datalist: await this.prisma.anime.findMany({
-        skip,
-        take,
-        cursor,
-        where,
-        orderBy,
-      }),
-      total: await this.prisma.anime.count(),
+        return this.prisma.anime.create({
+            data: {
+                ...rest,
+                genres: genres 
+                    ? {
+                        connect: genres?.map((id) => ({ id })),
+                    }
+                    : undefined,
+                studios: studios 
+                    ? {
+                        connect: studios?.map((id) => ({ id })),
+                    }
+                    : undefined,
+            },
+            include: {
+                genres: true,
+                studios: true,
+            },
+        });
     }
-  }
 
-  async findOne(animeWhereUniqueInput: Prisma.AnimeWhereUniqueInput): Promise<Anime | null> {
-    return this.prisma.anime.findUnique({
-      where: animeWhereUniqueInput,
-    });
-  }
+    async findAll(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.AnimeWhereUniqueInput;
+        where?: Prisma.AnimeWhereInput;
+        orderBy?: Prisma.AnimeOrderByWithRelationInput;
+    }): Promise<{ datalist: Anime[]; total: number }> {
+        const { skip, take, cursor, where, orderBy } = params;
+        const initDatalist = await this.prisma.anime.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+            include: { 
+                genres: { select: { id: true } }, 
+                studios: { select: { id: true } },
+            },
+        });
 
-  update(id: number, updateAnimeDto: UpdateAnimeDto) {
-    try {
-      const { genres, studios, ...rest } = updateAnimeDto;
-
-      return this.prisma.anime.update({
-        where: { id },
-        data: {
-          ...rest,
-          genres: genres 
-          ? {
-              set: genres?.map((id) => ({ id })),
-            }
-          : undefined,
-          studios: studios 
-          ? {
-              set: studios?.map((id) => ({ id })),
-            }
-          : undefined,
-        },
-        include: { 
-          genres: true,
-          studios: true,
+        return {
+            datalist: initDatalist.map(datalist => ({
+                ...datalist,
+                genres: datalist.genres.map(g => g.id),
+                studios: datalist.studios.map(s => s.id),
+            })),
+            total: await this.prisma.anime.count({
+                cursor,
+                where,
+                orderBy,
+            }),
         }
-      });
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
-        throw new NotFoundException(`Anime with ID ${id} not found`);
-      }
-      throw error;
     }
-    
-  }
+
+    async findOne(animeWhereUniqueInput: Prisma.AnimeWhereUniqueInput): Promise<Anime | null> {
+        return this.prisma.anime.findUnique({
+            where: animeWhereUniqueInput,
+        });
+    }
+
+    update(id: number, updateAnimeDto: UpdateAnimeDto) {
+        try {
+        const { genres, studios, ...rest } = updateAnimeDto;
+
+        return this.prisma.anime.update({
+            where: { id },
+            data: {
+                ...rest,
+                genres: genres 
+                    ? {
+                        set: genres?.map((id) => ({ id })),
+                        }
+                    : undefined,
+                studios: studios 
+                    ? {
+                        set: studios?.map((id) => ({ id })),
+                        }
+                    : undefined,
+            },
+            include: { 
+                genres: true,
+                studios: true,
+            }
+        });
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+                throw new NotFoundException(`Anime with ID ${id} not found`);
+            }
+            throw error;
+        }
+    }
 
   remove(id: number) {
     return `This action removes a #${id} anime`;
